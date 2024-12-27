d4ba33c6804de87afad317040560557927b28ec8
ShadmanAhm3d <shadman01010@gmail.com>
2024-12-27 13:16:59 +0530

    PP

--- basic/arrays/main.go
+++ basic/arrays/main.go
@@ -2,29 +2,40 @@ package main
 
 import "fmt"
 
-
-func someFunction (arr *[4]int){
-  arr[2] = 2;
-  fmt.Printf("Likh diya");
+func someFunction(arr *[4]int) {
+	arr[2] = 2
+	fmt.Printf("Likh diya")
 }
 
-func main(){
+func main() {
 
-  var arr[4] int ;
+	var arr [4]int
 
-  for i :=0; i <4 ; i++ {
-    fmt.Printf("Enter element number %d : ", i);
-    //input element dynamically ? 
-    fmt.Scan(&arr[i]);
-  }
+	for i := 0; i < 4; i++ {
+		fmt.Printf("Enter element number %d : ", i)
+		//input element dynamically ?
+		fmt.Scan(&arr[i])
+	}
 
-  for i:=0 ; i<len(arr);i++{
-    fmt.Printf("%d",arr[i]);
-    fmt.Print("\n");
-  }
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%d", arr[i])
+		fmt.Print("\n")
+	}
 
-  someFunction(&arr) //why am i getting error here 
+	someFunction(&arr) //why am i getting error here
 
-  fmt.Println(arr[2])
+	fmt.Println("Range based looping : ")
+	for _, elem := range arr {
+		fmt.Println("ELement  : ", elem)
+	}
+
+	fmt.Println("Formal Looping")
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Print(arr[i])
+		fmt.Println()
+	}
+
+	fmt.Println("Length of arr from lens : ", len(arr))
 
 }
